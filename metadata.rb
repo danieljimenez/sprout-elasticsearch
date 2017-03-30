016712459c6dcda1b147f349a04422820219405f
Daniel Jimenez <daniel.j.jimenez@gmail.com>
2017-03-30 14:30:32 -0500

    hack to fix old brew version missing

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'danielj@foreflight.com'
 license          'MIT'
 description      'Recipes to install and configure ElasticSearch on OS X'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 supports         'mac_os_x'
 depends          'homebrew', '>= 1.5.4'
--- recipes/install_elasticsearch.rb
+++ recipes/install_elasticsearch.rb
@@ -1,4 +1,9 @@
 include_recipe 'homebrew'
 include_recipe 'java'
 
-homebrew_package "elasticsearch@#{node['sprout']['elasticsearch']['version']['long']}"
+cookbook_file "elasticsearch@#{node['sprout']['elasticsearch']['version']['long']}.rb" do
+  source 'elasticsearch@1.7.rb'
+  path "#{Chef::Config['file_cache_path']}/elasticsearch@1.7.rb"
+end
+
+homebrew_package "#{Chef::Config['file_cache_path']}/elasticsearch@#{node['sprout']['elasticsearch']['version']['long']}.rb"
